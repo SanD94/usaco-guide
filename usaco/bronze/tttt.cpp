f3b6380fe44d30ee9f317c959642b80bdbbeca63
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-07-18 23:36:24 +0200

    simplify usaco bronze tttt

--- usaco/bronze/tttt.cpp
+++ usaco/bronze/tttt.cpp
@@ -1,104 +1,76 @@
 // https://usaco.org/index.php?page=viewproblem2&cpid=831
-#include <iostream>
-#include <array>
-#include <iterator>
 #include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
 #include <numeric>
 using namespace std;
 
+array<string, 3> ttt;
+
+int cow_wins(char ch) {
+  if (ch == ttt[0][0] && ch == ttt[1][1] && ch == ttt[2][2])
+    return 1;
+  if (ch == ttt[0][2] && ch == ttt[1][1] && ch == ttt[2][0])
+    return 1;
+
+  for (int i = 0; i < 3; i++) {
+    if (ch == ttt[i][0] && ch == ttt[i][1] && ch == ttt[i][2])
+      return 1;
+    if (ch == ttt[0][i] && ch == ttt[1][i] && ch == ttt[2][i])
+      return 1;
+  }
+  return 0;
+}
+
+bool check3(char ch1, char ch2, char a, char b, char c) {
+  if (a != ch1 && a != ch2) return false;
+  if (b != ch1 && b != ch2) return false;
+  if (c != ch1 && c != ch2) return false;
+  
+  if (ch1 != a && ch1 != b && ch1 != c) return false;
+  if (ch2 != a && ch2 != b && ch2 != c) return false;
+  
+  return true;
+}
+
+int team_wins(char ch1, char ch2) {
+  if (check3(ch1, ch2, ttt[0][0], ttt[1][1], ttt[2][2]))
+    return 1;
+  if (check3(ch1, ch2, ttt[0][2], ttt[1][1], ttt[2][0]))
+    return 1;
+
+  for (int i = 0; i < 3; i++) {
+    if (check3(ch1, ch2, ttt[i][0], ttt[i][1], ttt[i][2]))
+      return 1;
+    if (check3(ch1, ch2, ttt[0][i], ttt[1][i], ttt[2][i]))
+      return 1;
+  }
+
+  return 0;
+}
+
 void set_io(string s) {
-    freopen((s + ".in").c_str(), "r", stdin);
-    freopen((s + ".out").c_str(), "w", stdout);
+  freopen((s + ".in").c_str(), "r", stdin);
+  freopen((s + ".out").c_str(), "w", stdout);
 }
 
 int main(int argc, char const *argv[]) {
-    set_io("tttt");
-    array<string, 3> ttt;
-    copy(istream_iterator<string>(cin), istream_iterator<string>(), ttt.begin());
-    
-    // single claim
-    array<bool, 26> single{};
-    for (int i = 0; i < 26; i++) {
-        char ch = 'A' + i;
-        // row 
-        for (int j = 0; j < 3; j++) {
-            bool ok = true;
-            for (int k = 0; k < 3; k++) if (ttt[j][k] != ch) ok = false;
-            if (ok) single[i] = true;
-        }
-        // column
-        for (int j = 0; j < 3; j++) {
-            bool ok = true;
-            for (int k = 0; k < 3; k++) if (ttt[k][j] != ch) ok = false;
-            if (ok) single[i] = true;
-        }
-        // diagonal
-        bool ok = true;
-        for (int j = 0; j < 3; j++) {
-            if (ttt[j][j] != ch) ok = false;
-        }
-        if (ok) single[i] = true;
-        for (int j = 0; j < 3; j++) {
-            if (ttt[j][3-j] != ch) ok = false;
-        }
-        if (ok) single[i] = true;
-    }
-    int res = accumulate(single.begin(), single.end(), 0, plus<int>());
-    cout << res << endl;
+  set_io("tttt");
+  copy(istream_iterator<string>(cin), istream_iterator<string>(), ttt.begin());
 
-    // team claim
-    array<array<bool, 26>, 26> team{};
-    for (int i = 0; i < 26; i++)
-        for(int j = i + 1; j < 26; j++) {
-            if (i == j) continue;
-            char ch1 = i + 'A', ch2 = j + 'A';
-            
-            // row
-            for (int k = 0; k < 3; k++) {
-                int cnt1 = 2, cnt2 = 2;
-                for (int l = 0; l < 3; l++)  {
-                    if (ttt[k][l] == ch1) cnt1 --;
-                    if (ttt[k][l] == ch2) cnt2 --;
-                }
-                if (cnt1 >= 0 && cnt2 >= 0 && cnt1 + cnt2 == 1)
-                    team[i][j] = true;
-            }
-            // column
-            for (int k = 0; k < 3; k++) {
-                int cnt1 = 2, cnt2 = 2;
-                for (int l = 0; l < 3; l++)  {
-                    if (ttt[l][k] == ch1) cnt1 --;
-                    if (ttt[l][k] == ch2) cnt2 --;
-                }
-                if (cnt1 >= 0 && cnt2 >= 0 && cnt1 + cnt2 == 1)
-                    team[i][j] = true;
-            }
-            // diag
-            int cnt1 = 2, cnt2 = 2;
-            for (int k = 0; k < 3; k++) {
-                if (ttt[k][k] == ch1) cnt1--;
-                if (ttt[k][k] == ch2) cnt2--;
-            }
-            if (cnt1 >= 0 && cnt2 >= 0 && cnt1 + cnt2 == 1)
-                team[i][j] = true;
-            cnt1 = 2, cnt2 = 2;
-            for (int k = 0; k < 3; k++) {
-                if (ttt[k][k] == ch1) cnt1--;
-                if (ttt[k][k] == ch2) cnt2--;
-            }
-            if (cnt1 >= 0 && cnt2 >= 0 && cnt1 + cnt2 == 1)
-                team[i][j] = true;
-            cnt1 = 2, cnt2 = 2;
-            for (int k = 0; k < 3; k++) {
-                if (ttt[k][3-k] == ch1) cnt1--;
-                if (ttt[k][3-k] == ch2) cnt2--;
-            }
-            if (cnt1 >= 0 && cnt2 >= 0 && cnt1 + cnt2 == 1)
-                team[i][j] = true;
-        }
-    res = 0;
-    for(auto &arr : team) for (auto &elem: arr) if (elem) res++;
-    cout << res << endl;
+  // single claim
+  int res = 0;
+  for (int i = 0; i < 26; i++)
+    res += cow_wins('A' + i);
+  cout << res << endl;
 
-    return 0;
+  // team claim
+  res = 0;
+  for (int i = 0; i < 26; i++)
+    for (int j = i+1; j < 26; j++)
+      res += team_wins('A' + i, 'A' + j);
+  cout << res << endl;
+
+  return 0;
 }
