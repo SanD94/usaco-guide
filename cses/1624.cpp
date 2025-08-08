d5570d6498731bd72eb5dcc2ae9f44f6a987098b
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-08-08 09:53:03 +0200

    improve cses chessboard

--- cses/1624.cpp
+++ cses/1624.cpp
@@ -5,23 +5,14 @@
 using namespace std;
 
 bool eval(vector<int> &queens, vector<vector<bool>> &mark) {
-  int x, y, ix, iy;
+  int x, y;
+  vector<bool> ldiag(15, false), rdiag(15, false);
   for (int i = 0; i < 8; i++) {
     x = i, y = queens[i];
-    if (mark[x][y]) return false;
-    // left diag
-    ix = x - min(x, y) - 1, iy = y - min(x, y) - 1;
-    while (ix++ < 7 && iy++ < 7) {
-      if (ix == x && iy == y) continue;
-      if (queens[ix] == iy) return false;
-    }
-    // right diag
-    ix = x - min(x, 7 - y) - 1, iy = y + min(x, 7 - y) + 1;
-    while (ix++ < 7 && iy-- > 0) {
-      if (ix == x && iy == y) continue;
-      if (queens[ix] == iy) return false;
-    }
-    
+    if (mark[x][y] || ldiag[x + y] || rdiag[x - y + 7])
+      return false;
+    ldiag[x + y] = true;
+    rdiag[x - y + 7] = true;
   }
   return true;
 }
@@ -34,14 +25,15 @@ int main(int argc, char const *argv[]) {
   for (int i = 0; i < 8; i++) {
     cin >> s;
     placed.emplace_back(i);
-    for (int j = 0; j < 8; j++) reserved[i][j] = s[j] == '*';
+    for (int j = 0; j < 8; j++)
+      reserved[i][j] = s[j] == '*';
   }
-  
+
   int res = 0;
   do {
     res += eval(placed, reserved);
-  } while(next_permutation(placed.begin(), placed.end()));
-  
+  } while (next_permutation(placed.begin(), placed.end()));
+
   cout << res << endl;
   return 0;
 }
