1ae8e1c0d06c2f9941698cba08061d9e52eb95f6
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-08-10 19:31:47 +0200

    improve cf 581D

--- cf/581D.cpp
+++ cf/581D.cpp
@@ -18,85 +18,48 @@ struct point {
   }
 };
 
-
-bool found = false;
-
-void print(vector<point> &ps, vector<point> logos, int sz) {
-  if (found) return;
-  vector<vector<char>> board(sz, vector<char>(sz));
-  for (const auto &p : ps) 
-    if (p.c != 0) {
-      int lx, ly;
-      char c;
-      if (p.c > 3) lx = logos[p.c - 4].y, ly = logos[p.c - 4].x, c = logos[p.c - 4].c;
-      else lx = logos[p.c - 1].x, ly = logos[p.c - 1].y, c = logos[p.c - 1].c;
-      for (int i = p.x; i < p.x + lx; i++)
-        for(int j = p.y; j < p.y + ly; j++)
-          board[j][i] = c;
-    }
-  cout << sz << endl;
-  for (int i = 0; i < sz; i++) {
-    for(int j = 0; j < sz; j++) cout << board[i][j];
-    cout << endl;
-  }
-  found = true;
-}
-
-void place(vector<point> ps, int idx, vector<point> logos) {
-  if (idx == 3) {
-    
-    int tx = 0, ty = 0;
-    vector<vector<int>> mark(301, vector<int>(301, 0));
-    for (const auto &p : ps) 
-      if (p.c != 0) {
-        int lx, ly;
-        if (p.c > 3) lx = logos[p.c - 4].y, ly = logos[p.c - 4].x;
-        else lx = logos[p.c - 1].x, ly = logos[p.c - 1].y;
-        for (int i = p.x; i < p.x + lx; i++)
-          for(int j = p.y; j < p.y + ly; j++)
-            mark[i][j]++;
-        tx = max(tx, p.x + lx);
-        ty = max(ty, p.y + ly);
-      }
-    bool is_found = true;
-    if (tx != ty) is_found = false;
-    for(int i = 0; i < tx; i++)
-      for(int j = 0; j < ty; j++)
-        if (mark[i][j] != 1) is_found = false;
-    
-    if (is_found) print(ps, logos, tx);
-    return;
-  }
-  if (found) return;
-  int tx, ty;
-  for (int i = 0; i < ps.size(); i++) {
-    if (ps[i].c == 0) {
-      // normal
-      ps[i].c = logos[idx].c - 'A' + 1;
-      tx = ps[i].x + logos[idx].x;
-      ty = ps[i].y + logos[idx].y;
-      ps.push_back({tx, ps[i].y, 0});
-      ps.push_back({ps[i].x, ty, 0});
-      place(ps, idx + 1, logos);
-      ps.pop_back(); ps.pop_back();
-      ps[i].c = 0;
-      // t
-      ps[i].c = logos[idx].c - 'A' + 4;
-      tx = ps[i].x + logos[idx].y;
-      ty = ps[i].y + logos[idx].x;
-      ps.push_back({tx, ps[i].y, 0});
-      ps.push_back({ps[i].x, ty, 0});
-      place(ps, idx + 1, logos);
-      ps.pop_back(); ps.pop_back();
-      ps[i].c = 0;
-    }
-  }
-}
-
 bool check(vector<point> &logos) {
-  place(vector<point>{{0, 0, 0}}, 0, logos);
-  return found;
+  // all top
+  int szx  = logos[0].x, szy = 0;
+  for (int i = 0; i < 3; i++) szy += logos[i].y;
+  if (szx == logos[1].x && szx == logos[2].x && szx == szy) {
+    cout << szx << endl;
+    for (int i = 0; i < logos.size(); i++) 
+      for (int j = 0; j < logos[i].y; j ++) {
+        for(int k = 0; k < logos[i].x; k++)
+          cout << logos[i].c;
+        cout << endl;
+      }
+    return true; 
+  }
+  szx = logos[0].x, szy = 0;
+  if (
+    szx == logos[1].x + logos[2].x &&
+    logos[1].y == logos[2].y &&
+    szx == logos[0].y + logos[1].y
+  ) {
+    cout << szx << endl;
+    for (int i = 0; i < logos[0].y; i++) {
+      for(int j = 0; j < logos[0].x; j++)
+        cout << logos[0].c;
+      cout << endl;
+    }
+    for (int i = 0; i < logos[1].y; i++) {
+      for (int j = 0; j < logos[1].x; j++)
+        cout << logos[1].c;
+      for (int j = 0; j < logos[2].x; j++)
+        cout << logos[2].c;
+      cout << endl;
+    }
+    return true;
+  }
+
+  return false;
 }
+
+
+
+
 
 int main(int argc, char const *argv[]) {
   vector<point> logos(3);
@@ -106,7 +69,14 @@ int main(int argc, char const *argv[]) {
   }
   sort(logos.begin(), logos.end());
   do {
-    if (check(logos)) return 0;
+
+    for (int i = 0; i < (1 << 3); i++) {
+      for (int j = 0; j < 3; j ++)
+        if (i & (1 << j)) swap(logos[j].x, logos[j].y);
+      if (check(logos)) return 0;
+      for (int j = 0; j < 3; j ++)
+        if (i & (1 << j)) swap(logos[j].x, logos[j].y);
+    }
   } while(next_permutation(logos.begin(), logos.end()));
 
   cout << -1 << endl;
