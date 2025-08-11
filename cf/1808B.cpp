2d365d653eb9670b32bddb92a426627aec857f88
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-08-11 08:08:52 +0200

    start cf 1808B

---
+++ cf/1808B.cpp
@@ -0,0 +1,30 @@
+// https://cses.fi/problemset/task/1621
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+#define all(x) begin((x)), end((x))
+using lli = long long int;
+
+int main(int argc, char const *argv[]) {
+  int t, N, M;
+  cin >> t;
+  while (t--) {
+    cin >> N >> M;
+    lli res = 0;
+    vector<vector<lli>> deck(M, vector<lli>(N));
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++)
+        cin >> deck[j][i];
+    }
+    for (int i = 0; i < M; i++)
+      sort(all(deck[i]));
+    for (int i = 0; i < M; i++)
+      for (int j = 0; j < N - 1; j++)
+        res += (deck[i][j + 1] - deck[i][j]) * (N - 1);
+
+    cout << res << endl;
+  }
+  return 0;
+}
