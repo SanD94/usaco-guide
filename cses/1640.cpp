6cd819deda7f92930e82a5515752a3d398aa9d54
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-08-11 21:24:06 +0200

    improve cses sum two vals

--- cses/1640.cpp
+++ cses/1640.cpp
@@ -1,31 +1,21 @@
 // https://cses.fi/problemset/task/1640
-#include <algorithm>
 #include <iostream>
-#include <iterator>
 #include <map>
-#include <vector>
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  map<int, vector<int>> vals;
-  int n, t, v;
+  map<int, int> vals;
+  int n, t, k;
   cin >> n >> t;
   for (int i = 0; i < n; i++) {
-    cin >> v;
-    vals[v].emplace_back(i + 1);
-  }
-
-  for (const auto &[k, v] : vals)
+    cin >> k;
     if (vals.count(t - k)) {
-      if (t == 2 * k) {
-        if (v.size() < 2)
-          continue;
-        cout << v[0] << " " << v[1] << endl;
-        return 0;
-      }
-      cout << v[0] << " " << vals[t - k][0] << endl;
+      cout << vals[t - k] << " " << i + 1 << endl;
       return 0;
     }
+    vals[k] = i + 1;
+  }
+
   cout << "IMPOSSIBLE" << endl;
 
   return 0;
