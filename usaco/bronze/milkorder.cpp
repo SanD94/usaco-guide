42f1d423ca946cccd837dac42617b33aa1537df8
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-08-14 14:39:57 +0200

    improve usaco bronze milkorder

--- usaco/bronze/milkorder.cpp
+++ usaco/bronze/milkorder.cpp
@@ -10,75 +10,63 @@ void set_io(string s) {
   freopen((s + ".out").c_str(), "w", stdout);
 }
 
-bool correct_order(vector<int> &placement, const vector<int> &order,
-                   vector<int> &mark) {
-  bool res = true;
-  for (int i = 0, pi = 1; i < order.size() && pi < placement.size();
-       i++, pi++) {
-    if (mark[order[i]]) { // already fixed
-      while (pi < placement.size() && placement[pi] != order[i])
-        pi++;
+// First sol:
+// simulating the ordered ones every time is hard to debug.
+// it mainly depends on pi iteration, one by one
+// lots of edge cases : check fixed one and no place available
+// you have to clear everything up, making debug much harder.
 
-      if (pi == placement.size() &&
-          placement[pi - 1] != order[i]) { // fixed one not found
-        res = false;
-        break;
-      }
+// Second sol:
+// similar to first, go through order but jump from one to another when it's
+// fixed and the current fix one should not have lower pos than current pi. If
+// not fixed, find a place, behave as if it's placed. no need for correction as
+// there is no assignment
+bool in_order(const vector<int> &order, const vector<int> &cowp,
+              const vector<int> &pcow) {
+  int pi = 1;
+  for (const auto cow : order) {
+    if (cowp[cow]) {
+      if (cowp[cow] < pi)
+        return false;
+      pi = cowp[cow];
       continue;
     }
-
-    while (pi < placement.size() && placement[pi])
+    while (pi < pcow.size() && pcow[pi])
       pi++;
-
-    if (pi == placement.size()) { // no place available
-      res = false;
-      break;
-    }
-
-    placement[pi] = order[i];
-    mark[order[i]] = 2;
+    if (pi == pcow.size())
+      return false;
+    pi++;
   }
-
-  for (int j = 1; j < placement.size(); j++)
-    if (mark[placement[j]] == 2) {
-      mark[placement[j]] = 0;
-      placement[j] = 0;
-    }
-
-  return res;
+  return true;
 }
 
 int main(int argc, char const *argv[]) {
   set_io("milkorder");
-  int N, M, K, c, p, res = -1;
+  int N, M, K, c, p, res = 0;
   bool found = false;
   cin >> N >> M >> K;
 
-  vector<int> placements(N + 1, 0), order;
-  vector<int> mark(N + 1, 0);
+  // cow i located at p, position i cow resides
+  vector<int> cowp(N + 1, 0), pcow(N + 1, 0);
+  vector<int> order;
 
   copy_n(istream_iterator<int>(cin), M, back_inserter(order));
   for (int i = 0; i < K; i++) {
     cin >> c >> p;
-    placements[p] = c;
-    mark[c] = 1; // mark cow
-    if (c == 1) {
-      found = true;
-      res = p;
-    }
+    cowp[c] = p;
+    pcow[p] = c;
   }
+  res = cowp[1];
 
-  for (int i = 1; i <= N && !found; i++) {
-    if (placements[i])
+  for (int i = 1; i <= N && !res; i++) {
+    if (pcow[i])
       continue;
-    placements[i] = 1;
-    mark[1] = 1;
-    if (correct_order(placements, order, mark)) {
-      found = true;
+    pcow[i] = 1;
+    cowp[1] = i;
+    if (in_order(order, cowp, pcow))
       res = i;
-    }
-    placements[i] = 0;
-    mark[1] = 0;
+
+    pcow[i] = 0;
   }
   cout << res << endl;
   return 0;
