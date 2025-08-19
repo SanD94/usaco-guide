f4f5105ceef01877fb840c0184a7a6d211f38f6f
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-08-19 10:12:30 +0200

    improve usaco bronze eophotos

--- usaco/bronze/eophotos.cpp
+++ usaco/bronze/eophotos.cpp
@@ -1,17 +1,9 @@
 // https://usaco.org/index.php?page=viewproblem2&cpid=1084
-#include <algorithm>
 #include <iostream>
-#include <iterator>
 using namespace std;
 
-void set_io(string s) {
-  freopen((s + ".in").c_str(), "r", stdin);
-  freopen((s + ".out").c_str(), "w", stdout);
-}
-
 int main(int argc, char const *argv[]) {
-  int N, even = 0, odd = 0, cid, res = 0;
-  bool turn = true;
+  int N, even = 0, odd = 0, cid;
 
   cin >> N;
   for (int i = 0; i < N; i++) {
@@ -19,24 +11,20 @@ int main(int argc, char const *argv[]) {
     (cid % 2) ? odd++ : even++;
   }
 
-  while (odd && even) {
-    turn ? even-- : odd--;
-    turn = !turn;
-    res++;
-  }
-  res += odd / 3 * 2;
-  if (!turn && odd % 3 == 1)
-    res++;
-  if (turn) {
-    if (odd % 3 == 1)
-      res--;
-    if (odd % 3 == 2)
-      res++;
-    if (even)
-      res++;
-  }
+  // First attempt was to think about consuming all and handle
+  // the remainders accordingly (like sim), which leaves one to handle
+  // different cases at once. (turn and even-odd pairs)
+  // instead try to make even and odd as close as possible
+  // 2 odd makes 1 even.
+  // e = o or e = o + 1 is a solution
 
-  cout << res << endl;
+  while (odd > even) {
+    odd -= 2;
+    even++;
+  }
+  even = min(even, odd + 1);
+
+  cout << even + odd << endl;
 
   return 0;
 }
