f7dd7f1c119df32ddf4a52811a574509837d3d67
Safa Andac <8544658+SanD94@users.noreply.github.com>
2026-06-01 22:21:09 +0200

    remove redundant part to solve directly for hoof paper scissors minus one

--- usaco/bronze/hpsone.cpp
+++ usaco/bronze/hpsone.cpp
@@ -10,36 +10,20 @@ vector<vector<int>> chart_map;
 
 int get_state(int i, int j) {
     if (j < i) return chart_map[i][j];
-    int x = chart_map[j][i];
-    if (x == 0) return 3;
-    if (x == 3) return 0;
-    return x;
-}
-
-vector<int> get_hand(int index) {
-    vector<int> res;
-    for (int i = 0; i < N; i++) {
-        int state = get_state(index, i);
-        res.push_back(state);
-    }
-    return res;
+    return 3 - chart_map[j][i];
 }
 
 
 int eval(int rhs, int lhs) {
-    vector<int> chartr = get_hand(rhs);
-    vector<int> chartl = get_hand(lhs);
-    vector<int> elem;
-
-    for (int i = 0; i < N; i++)
-        elem.push_back(chartr[i] | chartl[i]);
-
 
     int win_hand = 0;
-    for(auto e : elem)
-        if (e == 0) win_hand ++;
+    for (int i = 0; i < N; i++) {
+        int stater = get_state(rhs, i);
+        int statel = get_state(lhs, i);
+        if ((stater | statel) == 0) win_hand++;
+    }
 
-    return win_hand * elem.size() * 2 - win_hand * win_hand;
+    return win_hand * N * 2 - win_hand * win_hand;
 }
 
 int main(int argc, char const *argv[]) {
